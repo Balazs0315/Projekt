f33a79f1d579b87479c4b3b423cf84b03e282dd7
Balazs0315 <kovacs.balazs.0315@gmail.com>
2025-10-07 20:09:26 +0200

    szamoloteglatest

--- java.js
+++ java.js
@@ -13,27 +13,31 @@ function szamolo(){
     let b = document.getElementById("ertek2").value
     fixar = 1317075
 
-    let A = 2*(a*b+a*2,5+b*2,5)
-    let V = (a*b*2,5)
-    let VA = V/A
+    felszin = 2*(a*b+a*2.5+b*2.5)
+    terfogat = (a*b*2.5)
+    va = terfogat/felszin
 
-    let osszeg = fixar*VA
-    let eredmeny = document.getElementById("eredmeny").innerHTML = `${osszeg}`
+    osszeg = va*fixar
+
+    let eredmeny = document.getElementById("eredmeny").innerHTML = `${Math.round(osszeg)} Ft`
 }
 
 function teglatest(){
-    let teglaszmolo = document.getElementById("szamolo").innerHTML = `<form action="" id="urlap">
+    let teglaszmolo = document.getElementById("szamolo").innerHTML = 
+        `
+        <form action="" id="urlap">
             <img src="" alt="">
 
             <h3>Téglatest</h3>
 
             <label for="ertek">A oldal</label>
-            <input type="range" name="ertek" id="ertek" min="3" max="10" value="3" oninput="range1()"><span id="kiir">3 cm</span><br><br>
+            <input type="range" name="ertek" id="ertek" min="3" max="10" value="3" oninput="range1()"><span id="kiir">3 m</span><br><br>
 
             <label for="">B oldal</label>
-            <input type="range" name="ertek2" id="ertek2" min="2" max="10" value="2" oninput="range2()"><span id="kiir2">2 cm</span><br><br>
+            <input type="range" name="ertek2" id="ertek2" min="2" max="10" value="2" oninput="range2()"><span id="kiir2">2 m</span><br><br>
 
             <button type="button" onclick="szamolo()">Számolás</button><br><br>
             <span id="eredmeny"></span>
-        </form>`
+        </form>
+        `
 }
